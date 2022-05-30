7c7d0014ab059dbf65b010fbb5bd267558db21ac
Georgy-Smirnov <80692868+Georgy-Smirnov@users.noreply.github.com>
2022-05-30 19:57:05 +0300

    Update utility.hpp

--- ContainersSTL/utility.hpp
+++ ContainersSTL/utility.hpp
@@ -12,9 +12,9 @@ namespace ft {
 template<class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T&>
 struct iterator
 {
-    typedef T			value_type;
+    typedef T		value_type;
     typedef Distance	difference_type;
-    typedef Pointer		pointer;
+    typedef Pointer	pointer;
     typedef Reference	reference;
     typedef Category	iterator_category;
 };
@@ -25,29 +25,29 @@ struct iterator
 
 template <typename Iterator>
 struct iterator_traits {
-	typedef typename Iterator::value_type			value_type;
-	typedef typename Iterator::pointer			pointer;
-	typedef typename Iterator::reference			reference;
-	typedef typename Iterator::difference_type		difference_type;
-	typedef typename Iterator::iterator_category		iterator_category;
-	typedef typename Iterator::pointer					iterator_type;
-};
+	typedef typename Iterator::value_type		value_type;
+	typedef typename Iterator::pointer		pointer;
+	typedef typename Iterator::reference		reference;
+	typedef typename Iterator::difference_type	difference_type;
+	typedef typename Iterator::iterator_category	iterator_category;
+	typedef typename Iterator::pointer		iterator_type;
+}
 
 template <typename T>
 struct iterator_traits<T*> {
-	typedef T									value_type;
-	typedef T*									pointer;
-	typedef T&									reference;
-	typedef std::ptrdiff_t						difference_type;
+	typedef T					value_type;
+	typedef T*					pointer;
+	typedef T&					reference;
+	typedef std::ptrdiff_t				difference_type;
 	typedef std::random_access_iterator_tag		iterator_category;
 };
 
 template <typename T>
 struct iterator_traits<const T*> {
-	typedef T									value_type;
-	typedef T*									pointer;
-	typedef T&									reference;
-	typedef std::ptrdiff_t						difference_type;
+	typedef T					value_type;
+	typedef T*					pointer;
+	typedef T&					reference;
+	typedef std::ptrdiff_t				difference_type;
 	typedef std::random_access_iterator_tag		iterator_category;
 };
 
@@ -89,33 +89,33 @@ struct false_type : integral_constant<bool, false> {};
 
 
 template <class T>
-struct is_integral				: false_type {};
+struct is_integral			: false_type {};
 template <>
 struct is_integral<bool>		: true_type {};
 template <>
-struct is_integral<unsigned char>		: true_type {};
+struct is_integral<unsigned char>	: true_type {};
 template <>
 struct is_integral<signed char>		: true_type {};
 template <>
 struct is_integral<char>		: true_type {};
 template <>
-struct is_integral<char16_t>	: true_type {};
+struct is_integral<char16_t>		: true_type {};
 template <>
-struct is_integral<char32_t>	: true_type {};
+struct is_integral<char32_t>		: true_type {};
 template <>
 struct is_integral<wchar_t>		: true_type {};
 template <>
-struct is_integral<signed short>		: true_type {};
+struct is_integral<signed short>	: true_type {};
 template <>
-struct is_integral<unsigned short>		: true_type {};
+struct is_integral<unsigned short>	: true_type {};
 template <>
-struct is_integral<signed int>			: true_type {};
+struct is_integral<signed int>		: true_type {};
 template <>
-struct is_integral<unsigned int>			: true_type {};
+struct is_integral<unsigned int>	: true_type {};
 template <>
 struct is_integral<signed long>		: true_type {};
 template <>
-struct is_integral<unsigned long>		: true_type {};
+struct is_integral<unsigned long>	: true_type {};
 template <>
 struct is_integral<signed long long>	: true_type {};
 template <>
@@ -128,9 +128,8 @@ struct is_integral<unsigned long long>	: true_type {};
 template<class InputIt1, class InputIt2>
 bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2) {
 	for (; first1 != last1; ++first1, ++first2) {
-		if (!(*first1 == *first2)) {
+		if (!(*first1 == *first2))
 			return false;
-		}
 	}
 	return true;
 }
@@ -151,10 +150,11 @@ bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, I
 template <typename T1, typename T2>
 struct pair {
 	typedef T1 first_type;
-    typedef T2 second_type;
+    	typedef T2 second_type;
 
 	T1	first;
 	T2	second;
+	
 	pair() : first(), second() {}
 	pair(const T1& x, const T2& y): first(x), second(y) {}
 	// pair(const T1& x): first(x), second() {}
@@ -206,23 +206,25 @@ bool operator<=(const ft::pair<T1, T2>& x, const ft::pair<T1, T2>& y) {
 template <typename T, typename Allocator> class vector;
 template <class Key, class T, class Compare, class Allocator> class map;
 template <class Key, class Compare, class Allocator> class set;
+	
 }
 
 namespace std {
-	template <class T, class A>
-	void swap(ft::vector<T, A> &v1, ft::vector<T, A> &v2 ) {
-		v1.swap(v2);
-	}
+	
+template <class T, class A>
+void swap(ft::vector<T, A> &v1, ft::vector<T, A> &v2 ) {
+	v1.swap(v2);
+}
 
-	template <class K, class T, class C, class A>
-	void swap(ft::map<K, T, C, A> &v1, ft::map<K, T, C, A> &v2 ) {
-		v1.swap(v2);
-	}
+template <class K, class T, class C, class A>
+void swap(ft::map<K, T, C, A> &v1, ft::map<K, T, C, A> &v2 ) {
+	v1.swap(v2);
+}
 
-	template <class K, class C, class A>
-	void swap(ft::set<K, C, A> &v1, ft::set<K, C, A> &v2 ) {
-		v1.swap(v2);
-	}
+template <class K, class C, class A>
+void swap(ft::set<K, C, A> &v1, ft::set<K, C, A> &v2 ) {
+	v1.swap(v2);
+}	
 	
 }
 
